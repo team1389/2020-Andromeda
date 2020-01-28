6f7bda62b86327b3a16d3f9e286869f703424dc3
Jonathan765 <55296705+Jonathan765@users.noreply.github.com>
2020-01-28 16:02:07 -0500

    Update Conveyor.java

--- src/main/java/frc/subsystems/Conveyor.java
+++ src/main/java/frc/subsystems/Conveyor.java
@@ -1,6 +1,24 @@
 package frc.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
 public class Conveyor extends SubsystemBase {
+    private TalonSRX conveyorMotor;
+
+    public Conveyor() {
+        conveyorMotor = new TalonSRX(RobotMap.CONVEYOR_MOTOR);
+
+        conveyorMotor.setInverted(true);
+    }
+
+    public void startConveying() {
+        conveyorMotor.set(ControlMode.PercentOutput, 0.5);
+    }
+
+    public void stopConveying() {
+        conveyorMotor.set(ControlMode.PercentOutput, 0);
+    }
 }
