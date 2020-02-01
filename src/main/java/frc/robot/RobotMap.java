c51e65e066ff33962411fd64a59c3e5dc2d90da6
Kenneth Miura <kenneth.t.miura@gmail.com>
2020-02-01 16:15:12 -0500

    untested Indexer final

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,11 +8,16 @@ public class RobotMap {
     public static int FRONT_SHOOTER_RIGHT;
     public static int BACK_SHOOTER_LEFT;
     public static int BACK_SHOOTER_RIGHT;
+
     public static int RIGHT_DRIVE_A = 3;
     public static int RIGHT_DRIVE_B = 4;
     public static int LEFT_DRIVE_A = 1;
     public static int LEFT_DRIVE_B = 2;
+
     public static int INTAKE_MOTOR;
     public static int CONVEYOR_MOTOR;
+    public static int INDEX_MOTOR;
 
+    //Sensors
+    public static int DIO_INDEX_BEAM_BREAK;
 }
--- src/main/java/frc/subsystems/Indexer.java
+++ src/main/java/frc/subsystems/Indexer.java
@@ -1,6 +1,5 @@
 package frc.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -8,29 +7,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Indexer extends SubsystemBase {   //The motor to stop the balls is a BAG Motor
-    public DigitalInput di;
+    public DigitalInput indexBeamBreak;
     public CANSparkMax indexMotor;
-//Forward and Backward is used to specify the percent power of the motor which we are using.
-    public double forward = 0.5;
-    public double backward = -0.5;
 
     public Indexer() {
-        di = new DigitalInput(0); //parameter is the port
+        indexBeamBreak = new DigitalInput(RobotMap.DIO_INDEX_BEAM_BREAK); //parameter is the port
         indexMotor = new CANSparkMax(RobotMap.INDEX_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
-
     }
 
-    public void checkerForward() {
-        //returns true if it is open
-        if (di.get()) {
-            indexMotor.set(forward);
-        }
+    public boolean ballDetected() {
+        return indexBeamBreak.get();
     }
-    public void checkerBackward() {
-        //returns true if it is open
-        if (di.get()) {
-            indexMotor.set(backward);
-        }
+
+    public void runForward() {
+        indexMotor.set(1);
+    }
+
+    public void runBackward() {
+        indexMotor.set(-0.5);
     }
 
 }
