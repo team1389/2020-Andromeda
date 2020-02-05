af939e45534f8573616721135bb9cda4567ac92c
MaxOhm420 <maxdemohm@gmail.com>
2020-02-05 17:50:49 -0500

    took out pid method, is this to your satisfaction?

--- src/main/java/frc/commands/TurnToAngle.java
+++ src/main/java/frc/commands/TurnToAngle.java
@@ -26,9 +26,6 @@ public class TurnToAngle extends CommandBase {
         }
     }
 
-    public void PID() {
-    }
-
     @Override
     public void execute() {
         error = targetAngle - drivetrain.ahrs.getAngle();
