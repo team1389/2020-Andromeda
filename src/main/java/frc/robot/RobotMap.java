b1caa51e52643e559431b51128601b82f0f71a6e
Kenneth Miura <kenneth.t.miura@gmail.com>
2020-02-11 15:43:04 -0500

    updated ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,31 +4,25 @@ package frc.robot;
  * define Hardware Ports in here
  */
 public class RobotMap {
-    public static int SHOOTER_TOP;
-    public static int SHOOTER_BOTTOM;
-
-
-    public static int RIGHT_DRIVE_LEADER = 1;
-    public static int RIGHT_DRIVE_FOLLOWER = 2;
-    public static int LEFT_DRIVE_LEADER = 3;
-    public static int LEFT_DRIVE_FOLLOWER = 4;
+    public static final int SHOOTER_TOP = 8;
+    public static final int RIGHT_DRIVE_LEADER = 1;
+    public static final int RIGHT_DRIVE_FOLLOWER = 2;
+    public static final int LEFT_DRIVE_LEADER = 3;
+    public static final int LEFT_DRIVE_FOLLOWER = 4;
     //Intake
-    public static int INTAKE_MOTOR = 11;
-    public static int INTAKE_RIGHT_FORWARD_SOLENOID;
-    public static int INTAKE_RIGHT_REVERSE_SOLENOID;
-    public static int INTAKE_LEFT_FORWARD_SOLENOID;
-    public static int INTAKE_LEFT_REVERSE_SOLENOID;
-
+    public static final int INTAKE_MOTOR = 11;
+    public static final int INTAKE_RIGHT_FORWARD_SOLENOID = 1;// FAKE VALUES FOR TESTING
+    public static final int INTAKE_RIGHT_REVERSE_SOLENOID = 2;// FAKE VALUES FOR TESTING
+    public static final int INTAKE_LEFT_FORWARD_SOLENOID = 3;// FAKE VALUES FOR TESTING
+    public static final int INTAKE_LEFT_REVERSE_SOLENOID = 4;// FAKE VALUES FOR TESTING
     //Conveyor
-    public static int CONVEYOR_MOTOR_BACK = 9;
-    public static int CONVEYOR_MOTOR_FRONT = 12;
-
-    public static int INDEX_MOTOR = 10;
-
+    public static final int CONVEYOR_MOTOR_BACK = 9;
+    public static final int CONVEYOR_MOTOR_FRONT = 12;
+    public static final int INDEX_MOTOR = 10;
     //Sensors
-    public static int DIO_SHOOTER_BEAM_BREAK;
-    public static int DIO_PRE_INDEX_BEAM_BREAK;
-
+    public static final int DIO_SHOOTER_BEAM_BREAK = 1; //FAKE VALUES FOR TESTING
+    public static final int DIO_PRE_INDEX_BEAM_BREAK = 2;// FAKE VALUES FOR TESTING
+    public static int SHOOTER_BOTTOM = 6;
     // Climber
     public static int CLIMBER_SPARK; //somewhere between 5 and 8
     public static int CLIMBER_RIGHT_FORWARD_SOLENOID;
