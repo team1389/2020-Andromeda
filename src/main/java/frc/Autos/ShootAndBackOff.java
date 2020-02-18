79d090596058a2a63370b18cf1b91dd2acdcb2bf
Kenneth Miura <kenneth.t.miura@gmail.com>
2020-02-18 17:36:36 -0500

    initial commit

---
+++ src/main/java/frc/Autos/ShootAndBackOff.java
@@ -0,0 +1,11 @@
+package frc.Autos;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+
+public class ShootAndBackOff extends CommandGroupBase {
+    @Override
+    public void addCommands(Command... commands) {
+
+    }
+}
