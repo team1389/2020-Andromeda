e02dec4bdf3325894cd1bb55cc8eb312250ba6f5
Aron S <siposbalint420@gmail.com>
2020-02-20 18:15:22 -0500

    Rough draft for two of the autos

---
+++ src/main/java/frc/Autos/ShootNControlPickUp.java
@@ -0,0 +1,26 @@
+package frc.Autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.commands.DriveDistance;
+import frc.commands.ShootWithSensors;
+import frc.commands.TurnToAngle;
+import frc.robot.Robot;
+
+import javax.swing.*;
+
+/**
+ *
+ * Shoots and picks up from the control panel
+ *
+ */
+public class ShootNControlPickUp extends SequentialCommandGroup {
+
+public ShootNControlPickUp(){
+addRequirements(Robot.drivetrain, Robot.shooter);
+addCommands(new ShootWithSensors(2000),new TurnToAngle(0,false), new DriveDistance(191.2), new ShootWithSensors(2000));
+
+
+
+}
+
+}
