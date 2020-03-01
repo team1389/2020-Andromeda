f6a17cbf21ec059bf0e9341a6997a7875e764940
Kenneth Miura <kenneth.t.miura@gmail.com>
2020-03-01 18:32:24 -0500

    Intake speed now hard coded

--- src/main/java/frc/commands/RunIntake.java
+++ src/main/java/frc/commands/RunIntake.java
@@ -17,7 +17,7 @@ public class RunIntake extends CommandBase {
 
     @Override
     public void execute() {
-        intake.runIntake(0.75);
+        intake.runIntake();
         Robot.conveyor.conveyorMotorFront.set(ControlMode.PercentOutput, 0.5);
         Robot.conveyor.conveyorMotorBack.set(ControlMode.PercentOutput, 0.5);
     }
--- src/main/java/frc/subsystems/Intake.java
+++ src/main/java/frc/subsystems/Intake.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Intake extends SubsystemBase {
+    private double intakePercent = 0.75;
     private DoubleSolenoid extender;
     private VictorSPX intakeMotor;
 
@@ -20,12 +21,13 @@ public class Intake extends SubsystemBase {
     public void extendIntake() {
         extender.set(DoubleSolenoid.Value.kForward);
     }
-    public void retractIntake(){
+
+    public void retractIntake() {
         extender.set(DoubleSolenoid.Value.kReverse);
     }
 
-    public void runIntake(double percent) {
-        intakeMotor.set(ControlMode.PercentOutput, percent);
+    public void runIntake() {
+        intakeMotor.set(ControlMode.PercentOutput, intakePercent);
     }
 
     public void stopIntaking() {
