1ad8423e5116b2d89de805141fd31ec59626ca32
frogtd129 <31412003+frogtd129@users.noreply.github.com>
2020-03-02 16:16:51 -0500

    Changed Names

--- src/main/java/frc/commands/MLMoveToBall.java
+++ src/main/java/frc/commands/MLMoveToBall.java
@@ -1,18 +1,17 @@
 package frc.commands;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-public class MLPID extends CommandBase {
+public class MLMoveToBall extends CommandBase {
     double kP;
     double kI;
     double kD;
     PIDController pid;
 
 
-    public MLPID() {
+    public MLMoveToBall() {
         addRequirements(Robot.ml);
         pid = new PIDController(0.5, 0.05, 0);
     }
@@ -21,7 +20,7 @@ public class MLPID extends CommandBase {
         double error = Robot.ml.movement();
         double power = pid.calculate(error,0);
 //        SmartDashboard.putNumber("ML error", error);
-        if (power < 0.05) {
+        if (Math.abs(power) < 0.05) {
             power += 0.6;
         }
         Robot.drivetrain.set(-power,power);
